2a5e33dcd9dd0307a4a0bed1b471780a7c1f282b
Wan Zhang <1194451658@qq.com>
2014-03-27 23:49:49 +0800

    add copyright

--- template/chromium-bsu-practice/Classes/ammo/Ammo.cpp
+++ template/chromium-bsu-practice/Classes/ammo/Ammo.cpp
@@ -1,4 +1,21 @@
 
+
+// Copyright 2014 Wanwan Zhang
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 #include "Ammo.h"
 #include "aircraft/Aircraft.h"
 
@@ -109,7 +126,6 @@ void Ammo::update(float time)
 	
 	if(shouldExplode)
 	{
-		CCLOG("Ammo shouldExplode !!");
 		// shouldExplode = true;
 		shouldReleased = true;
 		removeFromParent();
@@ -129,8 +145,6 @@ void Ammo::BeginContact(b2Contact* contact)
 
 			doDamageToGameObject(ga);
 			doDamageToGameObject(gb);
-
-			CCLOG("Ammo::BeginContact Called !");
 		}
 	}
 }
@@ -149,5 +163,5 @@ Ammo::~Ammo()
 
 Ammo::Ammo()
 {
-	CCLOG("Ammo::Ammo Called !");
+	// CCLOG("Ammo::Ammo Called !");
 }
