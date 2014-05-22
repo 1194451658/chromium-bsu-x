90f9e3f755d9b7ec30e7f15ad9901bdcd8417331
wanzhang <1194451658@qq.com>
2014-05-22 18:10:07 +0800

    !!! REMOVE COCOS2D-X FILES, ONLY LEFT GAME FILE !!!

--- template/chromium-bsu-practice/proj.win32/main.cpp
+++ template/chromium-bsu-practice/proj.win32/main.cpp
@@ -16,7 +16,6 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     AppDelegate app;
     CCEGLView* eglView = CCEGLView::sharedOpenGLView();
     eglView->setViewName("HelloCpp");
-    //eglView->setFrameSize(480, 320);
-	eglView->setFrameSize(600, 800);
+    eglView->setFrameSize(480, 320);
     return CCApplication::sharedApplication()->run();
 }
